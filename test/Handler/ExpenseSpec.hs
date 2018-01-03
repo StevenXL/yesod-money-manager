73ea454138b07b48f12e8e4343277cb16a3660ee
Steven Leiva <leiva.steven@gmail.com>
2018-01-02 23:44:11 -0600

    Add form for creating an expense.

---
+++ test/Handler/ExpenseSpec.hs
@@ -0,0 +1,10 @@
+module Handler.ExpenseSpec (spec) where
+
+import TestImport
+
+spec :: Spec
+spec = withApp $ do
+
+    describe "getExpenseR" $ do
+        error "Spec not implemented: getExpenseR"
+
